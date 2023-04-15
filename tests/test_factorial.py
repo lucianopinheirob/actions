62072cf2adf232a4aef1aa15c530044bdc6330b8
lucianopinheirob <lucianopinheiro15@gmail.com>
2023-04-14 22:06:41 -0300

    modif3

--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -1,4 +1,5 @@
 from factorial import factorial
+import json
 
 def test_factorial_zero():
     assert factorial(0) == 1
@@ -8,4 +9,29 @@ def test_factorial_one():
 
 def test_factorial_five():
     assert factorial(5) == 120
+
+results = {
+    "tests": [
+        {
+            "name": "test_factorial_zero",
+            "passed": True
+        },
+        {
+            "name": "test_factorial_one",
+            "passed": True
+        },
+        {
+            "name": "test_factorial_five",
+            "passed": True
+        }
+    ]
+}
+
+for test in results["tests"]:
+    try:
+        eval(test["name"] + "()")
+    except AssertionError:
+        test["passed"] = False
+
+print(json.dumps(results))
 
